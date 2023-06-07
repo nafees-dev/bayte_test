64b42e3987bb6479161860d3815b34133677abe1
Nafees <mn36505@gmail.com>
2023-06-08 00:50:24 +0500

    fixed required chnages

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const sliderContents = [
   },
   {
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    imageSrc: "https://randomuser.me/api/portraits/women/60.jpg",
+    imageSrc: "https://randomuser.me/api/portraits/men/60.jpg",
     name: "Jane Smith",
     profession: "Graphic Designer",
     cvLink: "https://example.com/jane-smith-cv.pdf"
   },
   {
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae molestiae magni animi ipsa! Adipisci a quisquam eligendi velit facere ratione, dolore numquam? Culpa, sit optio!",
-    imageSrc: "https://randomuser.me/api/portraits/women/91.jpg",
+    imageSrc: "https://randomuser.me/api/portraits/men/91.jpg",
     name: "Lady Johnson",
     profession: "Software Engineer",
     cvLink: "https://example.com/mark-johnson-cv.pdf"
@@ -26,59 +26,110 @@ const sliderContents = [
 // Define the Slider class
 class Slider {
   constructor(element) {
-    // Initialize configuration options
     const animationTime = parseInt(element.dataset.animationTime) || 3000;
     const autoplayInterval = parseInt(element.dataset.autoplayInterval) || 5000;
     const showNavigation = element.dataset.showNavigation === "true";
+    this.createSlides(element)
 
-    // Initialize instance variables
     this.slides = Array.from(element.querySelectorAll(".slide"));
-    this.dots = Array.from(element.querySelectorAll(".dot"));
+    this.dots = [];
+    this.dotContainer = null;
     this.prevArrow = element.querySelector(".prev");
+    // this.sliderWrapper = element.querySelector(".slider");
     this.nextArrow = element.querySelector(".next");
+    this.dotsWrapper = element.querySelector(".dots");
     this.currentSlide = 0;
     this.animationTime = animationTime;
     this.autoplayInterval = autoplayInterval;
     this.showNavigation = showNavigation;
 
-    // Attach event listeners
     this.prevArrow.addEventListener("click", () => this.navigatePrev());
     this.nextArrow.addEventListener("click", () => this.navigateNext());
-    this.dots.forEach((dot, index) => {
-      dot.addEventListener("click", () => this.navigateToSlide(index));
-    });
 
-    // Start autoplay if enabled
     if (this.autoplayInterval) {
       this.autoplayId = setInterval(() => this.navigateNext(), this.autoplayInterval);
     }
 
-    // Show navigation if enabled
     if (this.showNavigation) {
       this.prevArrow.style.display = "block";
       this.nextArrow.style.display = "block";
     }
 
-    // Populate the slides with content
     this.slides.forEach((slide, index) => {
       const content = sliderContents[index];
+      const dot = document.createElement("div");
+      // const secondWrapper = slide.querySelector(".sliderImage");
+      dot.classList.add("dot");
+      if(index===0) dot.classList.add("active");
+
       const img = slide.querySelector(".sliderImage");
       const name = slide.querySelector(".name");
-      const description = slide.querySelector(".description");;
+      const description = slide.querySelector(".description");
 
       img.src = content.imageSrc || "";
       img.alt = content.name;
       name.textContent = content.name;
       description.textContent = content.description;
+    
+      this.dots.push(dot);
+      // slide.style.opacity = index === 0 ? 1 : 0;
+      this.dotsWrapper.appendChild(dot);
+    });
+
+    this.dots.forEach((dot, index) => {
+      dot.addEventListener("click", () => this.navigateToSlide(index));
     });
   }
 
   showSlide(index) {
-    this.slides.forEach((slide) => slide.classList.remove("active"));
-    this.dots.forEach((dot) => dot.classList.remove("active"));
-
+    this.slides.forEach((slide) => { 
+      slide.style.opacity = 0; slide.classList.remove("active"); 
+    });
+    this.slides[index].style.opacity = 1;
+    // this.slides[index].style.width = "100%" ;
+    // this.slides[index].style.height = "100%" ;
     this.slides[index].classList.add("active");
+    // this.slides[index].classList.remove("hide");
+    this.dots.forEach((dot) => dot.classList.remove("active"));
     this.dots[index].classList.add("active");
+  }
+
+  createSlides(element){
+    // const controls = element.querySelector(".controls");
+    const firstWrapper = element.querySelector(".first-wrapper");
+    sliderContents.forEach((content, index) => {
+      const sliderWrapper = document.createElement("div");
+      sliderWrapper.classList.add("slide");
+      if(index === 0){ sliderWrapper.classList.add("active"); }
+      // create description
+      const sliderDescription = document.createElement("div");
+      sliderDescription.classList.add("description");
+      sliderDescription.textContent = content.description;
+      // create slider image
+      const sliderImage = document.createElement("img");
+      sliderImage.classList.add("sliderImage");
+      sliderImage.src = content.imageSrc;
+      // create name
+      const sliderName = document.createElement("div");
+      sliderName.classList.add("name");
+      sliderName.textContent = content.name;
+      // create profession
+      const sliderProfession = document.createElement("div");
+      sliderProfession.classList.add("profession");
+      sliderProfession.textContent = content.profession;
+      // create cv link
+      const sliderCvLink = document.createElement("button");
+      sliderCvLink.classList.add("view-cv-btn");
+      sliderCvLink.textContent = "View CV";
+      // append all elements to slider wrapper
+      sliderWrapper.appendChild(sliderDescription);
+      sliderWrapper.appendChild(sliderImage);
+      sliderWrapper.appendChild(sliderName);
+      sliderWrapper.appendChild(sliderProfession);
+      sliderWrapper.appendChild(sliderCvLink);
+
+      firstWrapper.appendChild(sliderWrapper);
+    });
   }
 
   navigateToSlide(index) {
@@ -91,6 +142,13 @@ class Slider {
     }
 
     this.showSlide(this.currentSlide);
+
+    clearInterval(this.autoplayId);
+    if (this.autoplayInterval) {
+      this.autoplayId = setTimeout(() => {
+        this.autoplayId = setInterval(() => this.navigateNext(), this.autoplayInterval);
+      }, this.animationTime);
+    }
   }
 
   navigatePrev() {
@@ -106,10 +164,8 @@ class Slider {
   }
 }
 
-// Initialize slider components on page load
 window.addEventListener("load", () => {
   const sliders = Array.from(document.querySelectorAll(".slider"));
-
   sliders.forEach((slider) => {
     new Slider(slider);
   });
